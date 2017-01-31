090b3b5c3242c925627d7739508c9969bdf9e216
Byeongsu <1002718@SKP1002718MN001.local>
2017-01-31 17:08:41 +0900

    package 변경 및 bin 파일 추가

---
+++ core/src/main/java/kafka/skplanet/mirrormaker/common/ClusterResourceListener.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package kafka.skplanet.mirrormaker.common;
+
+import kafka.skplanet.mirrormaker.clients.producer.KafkaProducer;
+import kafka.skplanet.mirrormaker.clients.producer.ProducerInterceptor;
+import kafka.skplanet.mirrormaker.common.metrics.MetricsReporter;
+import kafka.skplanet.mirrormaker.common.serialization.Deserializer;
+import kafka.skplanet.mirrormaker.common.serialization.Serializer;
+
+/**
+ * A callback interface that users can implement when they wish to get notified about changes in the Cluster metadata.
+ * <p>
+ * Users who need access to cluster metadata in interceptors, metric reporters, serializers and deserializers
+ * can implement this interface. The order of method calls for each of these types is described below.
+ * <p>
+ * <h4>Clients</h4>
+ * There will be one invocation of {@link ClusterResourceListener#onUpdate(ClusterResource)} after each metadata response.
+ * Note that the cluster id may be null when the Kafka broker version is below 0.10.1.0. If you receive a null cluster id, you can expect it to always be null unless you have a cluster with multiple broker versions which can happen if the cluster is being upgraded while the client is running.
+ * <p>
+ * {@link ProducerInterceptor} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be invoked after {@link ProducerInterceptor#onSend(ProducerRecord)}
+ * but before {@link ProducerInterceptor#onAcknowledgement(RecordMetadata, Exception)} .
+ * <p>
+ * {@link org.apache.kafka.clients.consumer.ConsumerInterceptor} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be invoked before {@link org.apache.kafka.clients.consumer.ConsumerInterceptor#onConsume(ConsumerRecords)}
+ * <p>
+ * {@link Serializer} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be invoked before {@link Serializer#serialize(String, Object)}
+ * <p>
+ * {@link Deserializer} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be invoked before {@link Deserializer#deserialize(String, byte[])}
+ * <p>
+ * {@link MetricsReporter} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be invoked after first {@link KafkaProducer#send(ProducerRecord)} invocation for Producer metrics reporter
+ * and after first {@link org.apache.kafka.clients.consumer.KafkaConsumer#poll(long)} invocation for Consumer metrics reporters. The reporter may receive metric events from the network layer before this method is invoked.
+ * <h4>Broker</h4>
+ * There is a single invocation {@link ClusterResourceListener#onUpdate(ClusterResource)} on broker start-up and the cluster metadata will never change.
+ * <p>
+ * KafkaMetricsReporter : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be invoked during the bootup of the Kafka broker. The reporter may receive metric events from the network layer before this method is invoked.
+ * <p>
+ * {@link MetricsReporter} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be invoked during the bootup of the Kafka broker. The reporter may receive metric events from the network layer before this method is invoked.
+ */
+public interface ClusterResourceListener {
+    /**
+     * A callback method that a user can implement to get updates for {@link ClusterResource}.
+     * @param clusterResource cluster metadata
+     */
+    void onUpdate(ClusterResource clusterResource);
+}
